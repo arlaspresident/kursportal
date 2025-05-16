879c6fe4d7b7bca6c41735880f02b92e27fc9990
arlaspresident <jankovic4901@gmail.com>
2025-05-16 12:09:05 +0200

    uppdaterat app config

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Course {
+  courseCode: string;
+  subjectCode: string;
+  level: string;
+  progression: string;
+  courseName: string;
+  points: number;
+  institutionCode: string;
+  subject: string;
+  syllabus: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  private jsonUrl = '/miun_courses.json'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.jsonUrl);
+  }
 }
