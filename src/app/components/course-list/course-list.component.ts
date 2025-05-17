3c1796217c872c3e6f1c862a54a1010245e7e0cd
arlaspresident <jankovic4901@gmail.com>
2025-05-17 10:30:44 +0200

    lagt till sökfunktion

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { CourseService, Course } from '../../services/course.service';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit{
   courses: Course[] = [];
+
+  searchTerm: string = '';
 
   constructor(private courseService: CourseService) {}
 
@@ -22,4 +25,11 @@ export class CourseListComponent implements OnInit{
       console.error('Fel vid inläsning:', error);
     });
   }
+
+get filteredCourses(): Course[] {
+  return this.courses.filter(course =>
+    course.courseName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+    course.courseCode.toLowerCase().includes(this.searchTerm.toLowerCase())
+  );
+}
 }
